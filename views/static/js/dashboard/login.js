a34073f23f71c83df99e852fa7683d03eb2fae16
xiaoming <xiaoming@xm.com>
2017-09-12 00:06:32 +0800

    01 项目基本搭建完成，requirejs也引入到项目中

---
+++ views/static/js/dashboard/login.js
@@ -0,0 +1,46 @@
+define(["jquery","cookie"],function($){
+  $(function () {
+    // 取消掉button按钮提交的功能，而改用form表单提交注册事件,改用JQ给表单提供的submit方式提交
+    $("form").submit(function () {
+      // 表单提交数据时，需要获取到表单里面用户输入的内容
+      var userName = $("#tc_name").val();
+      var pass = $("#tc_pass").val();
+
+//             由于数据库中有个空的用户名和空的密码，当二者为空的时候也可以登录，所以需要做判断，当输入为空时需要输入内容
+      if(userName.trim() == ""){
+        alert("请输入用户名");
+        return false;
+      }
+      if (pass.trim() == "") {
+        alert("请输入密码");
+        return false;
+      }
+      // 然后根据拿到的输入数据向后台提交，使用ajax的post方式提交
+//          数据接口是http://studyit.com/api/login
+      $.ajax({
+        url:"/api/login",
+        type:"post",
+        data:{
+          tc_name:userName,
+          tc_pass:pass
+        },
+        success:function (data) {
+          console.log(data);
+//                 当请求成功，数据成功返回时，执行跳转到首页
+          if(data.code == 200){
+//                   JSON.stringify()将对象转换成JSON字符串，因为cookie只能存储字符串，
+//                   $.cookie(键,值,{expires:数据存储有效天数,path:存储路径}) "/"表示根目录
+            $.cookie("userinfo",JSON.stringify(data.result),{expires:365,path:"/"});
+
+//                   将数据保存到根目录下的cookie中以后，在首页中也可以访问到
+            window.location.href = "index";
+          }
+        }
+      });
+
+
+
+      return false;
+    });
+  });
+});
